e6a4af04ca928d2f8a8e3ea8598eadcd648a1bbb
joffr <jktuole@yandex.ru>
2017-10-12 10:26:32 +0300

    commit

--- scripts/slider_info.js
+++
@@ -1,5 +0,0 @@
-function choose_delivery() {
-    document.getElementsByClassName('guarantee_info').style.display = 'block';
-    document.getElementsByClassName('delivery_info').style.display = 'none';
-    document.getElementsByClassName('credit_info').style.display = 'none';
-}
